3c661f33665958f531fdf5667b3122eed25864db
Jashwanth_Chennoori <99673635+jashu8096@users.noreply.github.com>
2024-09-12 20:32:22 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,391 @@
+import React, { useEffect, useState } from 'react';
+import './Home.css'
+
+function Home(){
+    
+   
+    return(
+
+        <>
+        <div className='main-div'>
+            <div>
+
+            <h1 className='main-heading'>
+            JobVault
+            </h1>
+            <div>
+                <div className='tabs-div'>
+                    <p className='tabs'>
+                        Job Card
+
+                    </p> 
+                    <p className='tabs'>
+                        Bookmark
+
+                    </p>
+
+                </div>
+
+                <div className='list-items'>
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+                  <div className='item'>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Title : </p>
+                        <p className='exp'>Satyam Home Care Services wants nannies and ward boys for patient care, housework and cooking."</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Place : </p>
+                        <p className='exp'>Hyderabad</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Salary : </p>
+                        <p className='exp'>₹18000 - ₹25000+</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Job Type : </p>
+                        <p className='exp'>ఆఫీస్ జాబ్</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Experience : </p>
+                        <p className='exp'>Less than 1 year</p>
+                      </div>
+
+                      <div className='flex-tile'>
+                        <p className='title'>Qualification : </p>
+                        <p className='exp'>Not Required</p>
+                      </div>
+
+
+
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                   
+
+
+
+
+
+                    
+
+
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Home
